27ad6bee5cf947484c755784e5d54411b382c42b
marta <13161523@alu.murciaeduca.es>
2024-11-25 09:52:31 +0100

    explicacion middleware

--- routes/curriculos.php
+++ routes/curriculos.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\CurriculoController;
+use App\Http\Middleware\MyMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('curriculos', [CurriculoController::class, 'getIndex']);
 
 Route::get('curriculos/show/{id}', [CurriculoController::class, 'getShow'])
+->middleware(MyMiddleware::class)
 ->where('id', '[0-9]+');
 
 Route::get('curriculos/create', [CurriculoController::class, 'getCreate']);
