bdccf2c595efb6b9b9e1bda415e91b1710a74f7b
marta <13161523@alu.murciaeduca.es>
2024-12-02 09:51:31 +0100

    Consultar datos Modelo

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,39 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Models\Estudiante;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'getHome']);
 
-Route::get('login', function() {
+Route::get('login', function () {
     return view('auth.login');
 });
 
-Route::get('logout', function() {
+Route::get('logout', function () {
     return 'Logout usuario';
 });
 
-Route::get('perfil/{id?}', function($id = null) {
-    return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
+Route::get('perfil/{id?}', function ($id = null) {
+    return $id ? 'Visualizar el currículo de ' . $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
 
+Route::get('pruebaDB/{id}', function ($id = null) {
+    // $estudiantes = Estudiante::all();
+    // foreach ($estudiantes as $estudiante) {
+    //     echo $estudiante->nombre . ' - ' .  $estudiante->ciclo . '<br />';
+    // }
+    // $estudiante = Estudiante::findOrFail($id);
+    // echo $estudiante->nombre . ' - '. $estudiante->ciclo . '<br>';
+    $estudiante = Estudiante::where('ciclo', 'C_'.$id)->firstOrFail();
+    echo $estudiante->nombre;
+});
 
 
 
-
-include __DIR__.'/actividades.php';
-include __DIR__.'/curriculos.php';
-include __DIR__.'/proyectos.php';
-include __DIR__.'/reconocimientos.php';
-include __DIR__.'/users.php';
+include __DIR__ . '/actividades.php';
+include __DIR__ . '/curriculos.php';
+include __DIR__ . '/proyectos.php';
+include __DIR__ . '/reconocimientos.php';
+include __DIR__ . '/users.php';
