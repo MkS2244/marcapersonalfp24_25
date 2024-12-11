bac91292dec540b160d67edeb7bc9937f941ae80
MkS2244 <13161523@alu.murciaeduca.es>
2024-12-11 08:56:13 +0100

    Gestionando proyectos desde BD

--- resources/views/proyectos/index.blade.php
+++ resources/views/proyectos/index.blade.php
@@ -13,13 +13,13 @@
                 <h3>{{ $proyecto['nombre'] }}</h3>
             </header>
             <p>
-                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
-                    http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}">
+                    http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}
                 </a>
             </p>
             <footer>
                 <ul class="actions">
-                    <li><a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getShow'], ['id' => $key] ) }}" class="button alt">Más info</a></li>
+                    <li><a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getShow'], ['id' => $proyecto->id] ) }}" class="button alt">Más info</a></li>
                 </ul>
             </footer>
         </section>
